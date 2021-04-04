7af7432c52b968fcae3c9657160e5cae01d9508f
julio-Cortes <jicortes1@miuandes.cl>
2021-04-03 23:32:39 -0400

    missing_format

--- app/src/main/java/com/example/budget_app/MainActivity.kt
+++ app/src/main/java/com/example/budget_app/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.budget_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Button
+import com.example.navtest.Spend
+import com.example.navtest.SpendRecyclerViewAdapter
+import com.example.navtest.spendsFactory
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var spendsRecyclerView: RecyclerView
+    lateinit var adapter: SpendRecyclerViewAdapter
+    lateinit var spends:MutableList<Spend>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        spendsRecyclerView = findViewById(R.id.spend_recycler_view)
+        spendsRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = SpendRecyclerViewAdapter()
+        spendsRecyclerView.adapter= adapter
+
+        val addOneSpendButton = findViewById<Button>(R.id.plus_one_button)
+        addOneSpendButton.setOnClickListener{
+            adapter.addOneCase(spend = spendsFactory(1)[0])
+        }
+        val addFiveButton = findViewById<Button>(R.id.plus_five_button)
+
+        addFiveButton.setOnClickListener{
+            adapter.addFiveCases(spends = spendsFactory(5) as MutableList<Spend>)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/budget_app/SpendFactory.kt
+++ app/src/main/java/com/example/budget_app/SpendFactory.kt
@@ -1,5 +1,12 @@
 package com.example.navtest
 
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.budget_app.R
 import java.text.NumberFormat
 import java.util.*
 import kotlin.random.Random
@@ -18,7 +25,46 @@ data class Spend(
     }
 }
 
-fun spendsFactory(number: Int): List<Spend> {
+class SpendRecyclerViewAdapter(): RecyclerView.Adapter<SpendRecyclerViewAdapter.SpendViewHolder>(){
+
+    var data = mutableListOf<Spend>()
+
+    inner class SpendViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+        fun bindView(item: Spend){
+            val category = view.findViewById<TextView>(R.id.category_text_view)
+            val amout = view.findViewById<TextView>(R.id.tota_amout_view)
+            amout.text = "$ "+item.getAmountFormat()
+            category.text = item.categoryTitle
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): SpendRecyclerViewAdapter.SpendViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.spends_recycler_view,parent,false)
+        return SpendViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SpendRecyclerViewAdapter.SpendViewHolder, position: Int) {
+        val item = data[position]
+        holder.bindView(item)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+    fun addOneCase(spend: Spend){
+        data.add(spend)
+        this.notifyDataSetChanged()
+    }
+    fun addFiveCases(spends: MutableList<Spend>){
+        data.addAll(spends)
+        this.notifyDataSetChanged()
+    }
+
+
+
+}
+
+
+fun spendsFactory(number: Int): MutableList<Spend> {
     val spends = mutableListOf<Spend>()
     val categories = listOf("bottle cap", "monitor", "charger", "soy sauce packet", "nail clippers", "drill press", "sofa", "tissue box", "bag", "fake flowers", "glow stick", "soap", "twister", "toothbrush", "video games", "rubber duck", "towel", "money", "milk", "rusty nail", "house", "toe ring", "pillow", "bananas", "mouse pad", "chair", "credit card", "doll", "screw", "thread", "flowers", "drawer", "pen", "paper", "magnet", "tomato", "glasses", "spoon", "chalk", "eraser", "eye liner", "cork", "button", "spring", "mp3 player", "tree", "car", "food", "bottle", "toilet", "ice cube tray", "nail file", "hanger", "bookmark", "bowl", "needle", "stop sign", "ring", "leg warmers", "plate", "headphones", "socks", "shoes", "lace", "shawl", "cinder block", "television", "bed", "water bottle", "fork", "tooth picks", "pencil", "bow", "candy wrapper", "cup", "sharpie", "clothes", "bracelet", "brocolli", "grid paper", "chocolate", "clamp", "helmet", "lamp shade", "rug", "lamp", "hair brush", "teddies", "shampoo", "speakers", "wallet", "lip gloss", "phone", "remote", "boom box", "beef", "picture frame", "shoe lace", "floor")
     for (i in 1..number) {
